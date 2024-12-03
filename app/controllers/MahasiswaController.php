b40c5ca9d43138d340bad2cb1efd7079339bc4c7
tasyasss <tasyasakina0703@gmail.com>
2024-12-03 14:14:41 +0700

    sidebar & pages mahasiswa

--- app/controllers/MahasiswaController.php
+++ app/controllers/MahasiswaController.php
@@ -13,4 +13,40 @@ class MahasiswaController extends Controller
         include '../app/views/mahasiswa/dashboard.php';
         include '../app/views/template/footer.php';
     }
+
+    public function profile()
+    {
+        $judul = 'Mahasiswa';
+        include '../app/views/template/header.php';
+        include '../app/views/template/sidebar_mahasiswa.php';    
+        include '../app/views/mahasiswa/profile.php';
+        include '../app/views/template/footer.php';
+    }
+
+    public function kompetisi()
+    {
+        $judul = 'Mahasiswa';
+        include '../app/views/template/header.php';
+        include '../app/views/template/sidebar_mahasiswa.php';    
+        include '../app/views/mahasiswa/kompetisi.php';
+        include '../app/views/template/footer.php';
+    }
+
+    public function setting()
+    {
+        $judul = 'Mahasiswa';
+        include '../app/views/template/header.php';
+        include '../app/views/template/sidebar_mahasiswa.php';    
+        include '../app/views/mahasiswa/setting.php';
+        include '../app/views/template/footer.php';
+    }
+
+    public function help()
+    {
+        $judul = 'Mahasiswa';
+        include '../app/views/template/header.php';
+        include '../app/views/template/sidebar_mahasiswa.php';    
+        include '../app/views/mahasiswa/help.php';
+        include '../app/views/template/footer.php';
+    }
 }
---
+++ app/views/mahasiswa/setting.php
@@ -0,0 +1,7 @@
+<div class="container">
+        <h1 class="mt-5">HALO, <?= $_SESSION['user']; ?></h1>
+        <p>Ini SETTINGGGGGG <?= $_SESSION['role']; ?>.</p>
+        <a href="<?= BASE_URL; ?>auth/logout">Logout</a>
+    </div>
+
+    
