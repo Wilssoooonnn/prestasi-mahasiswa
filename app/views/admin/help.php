11950eb4273b19b6acc2aecaea1ee6e2943e230f
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2024-12-06 09:28:14 +0700

    menambahkan halaman help di view admin

--- app/views/admin/help.php
+++ app/views/admin/help.php
@@ -1,7 +1,87 @@
+<div class="container mt-5">
+    <section class="section faq">
+        <div class="row">
+            <div class="col-lg-12">
 
+                <!-- F.A.Q Group 3 -->
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Dolore occaecati ducimus quam</h5>
 
-<div class="container">
-    <h1 class="mt-5">Help Page</h1>
-    <p>ini Help</p>
-    <a href="<?= BASE_URL; ?>auth/logout">Logout</a>
+                        <div class="accordion accordion-flush" id="faq-group-3">
+
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" data-bs-target="#faqsThree-1" type="button" data-bs-toggle="collapse">
+                                        Assumenda doloribus est fugiat sint incidunt animi totam nisi?
+                                    </button>
+                                </h2>
+                                <div id="faqsThree-1" class="accordion-collapse collapse" data-bs-parent="#faq-group-3">
+                                    <div class="accordion-body">
+                                        Ut quasi odit odio totam accusamus vero eius. Nostrum asperiores voluptatem eos nulla ab dolores est asperiores iure. Quo est quis praesentium aut maiores. Corrupti sed aut expedita fugit vero dolorem. Nemo rerum sapiente. A quaerat dignissimos.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" data-bs-target="#faqsThree-2" type="button" data-bs-toggle="collapse">
+                                        Consequatur saepe explicabo odio atque nisi?
+                                    </button>
+                                </h2>
+                                <div id="faqsThree-2" class="accordion-collapse collapse" data-bs-parent="#faq-group-3">
+                                    <div class="accordion-body">
+                                        In minus quia impedit est quas deserunt deserunt et. Nulla non quo dolores minima fugiat aut saepe aut inventore. Qui nesciunt odio officia beatae iusto sed voluptatem possimus quas. Officia vitae sit voluptatem nostrum a.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" data-bs-target="#faqsThree-3" type="button" data-bs-toggle="collapse">
+                                        Voluptates vel est fugiat molestiae rem sit eos sint?
+                                    </button>
+                                </h2>
+                                <div id="faqsThree-3" class="accordion-collapse collapse" data-bs-parent="#faq-group-3">
+                                    <div class="accordion-body">
+                                        Voluptates magni amet enim perspiciatis atque excepturi itaque est. Sit beatae animi incidunt eum repellat sequi ea saepe inventore. Id et vel et et. Nesciunt itaque corrupti quia ducimus. Consequatur maiores voluptatum fuga quod ut non fuga.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" data-bs-target="#faqsThree-4" type="button" data-bs-toggle="collapse">
+                                        Ab ipsa cum autem voluptas doloremque velit?
+                                    </button>
+                                </h2>
+                                <div id="faqsThree-4" class="accordion-collapse collapse" data-bs-parent="#faq-group-3">
+                                    <div class="accordion-body">
+                                        Numquam ut reiciendis aliquid. Quia veritatis quasi ipsam sed quo ut eligendi et non. Doloremque sed voluptatem at in voluptas aliquid dolorum.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" data-bs-target="#faqsThree-5" type="button" data-bs-toggle="collapse">
+                                        Aliquam magni ducimus facilis numquam dolorum harum eveniet iusto?
+                                    </button>
+                                </h2>
+                                <div id="faqsThree-5" class="accordion-collapse collapse" data-bs-parent="#faq-group-3">
+                                    <div class="accordion-body">
+                                        Aut necessitatibus maxime quis dolor et. Nihil laboriosam molestiae qui molestias placeat corrupti non quo accusamus. Nemo qui quis harum enim sed. Aliquam molestias pariatur delectus voluptas quidem qui rerum id quisquam. Perspiciatis voluptatem voluptatem eos. Vel aut minus labore at rerum eos.
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div><!-- End F.A.Q Group 3 -->
+
+            </div>
+
+        </div>
+    </section>
 </div>
