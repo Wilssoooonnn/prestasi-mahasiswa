5888d74db29496bf674d6026d32e31187f38a127
Wilssoooonnn <adamnuralifi11@gmail.com>
2024-12-06 08:37:44 +0700

    fix js, and add view

--- app/views/template/footer.php
+++ app/views/template/footer.php
@@ -1,8 +1,7 @@
-</body>
 <!-- Source JavaScript -->
-<script src="https://cdn.jsdelivr.net/npm/@coreui/coreui@5.2.0/dist/js/coreui.bundle.min.js" integrity="sha384-JdRP5GRWP6APhoVS1OM/pOKMWe7q9q8hpl+J2nhCfVJKoS+yzGtELC5REIYKrymn" crossorigin="anonymous"></script>
-<script src="<?= BASE_URL; ?>/public/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="<?= BASE_URL; ?>/public/assets/js/jquery.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@coreui/coreui@5.2.0/dist/js/coreui.bundle.min.js" integrity="sha384-JdRP5GRWP6APhoVS1OM/pOKMWe7q9q8hpl+J2nhCfVJKoS+yzGtELC5REIYKrymn" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 <!-- End Source JavaScript -->
 
@@ -14,5 +13,6 @@
     });
 </script>
 <!-- End Script Code -->
+</body>
 
 </html>
--- app/views/template/header.php
+++ app/views/template/header.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman <?= $judul; ?></title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- CoreUI CSS -->
     <link href="https://cdn.jsdelivr.net/npm/@coreui/coreui@5.2.0/dist/css/themes/bootstrap/bootstrap.min.css" rel="stylesheet" integrity="sha384-nQQlHXZO4YmST3YDqk/JU3f1t2D58a/nPd1QbiLecouKn68glzRym4BlOxlr5Rrg" crossorigin="anonymous">
     <!-- UI Icons -->
@@ -13,8 +15,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <!-- Local CSS -->
     <link rel="stylesheet" href="<?= BASE_URL; ?>public/assets/css/style.sidebar.css">
-    <!-- <link rel="stylesheet" href="<?= BASE_URL; ?>public/assets/css/style.login.css"> -->
-    <link rel="stylesheet" href="<?= BASE_URL; ?>public/assets/bootstrap/css/bootstrap.min.css">
 </head>
 <style>
     * {
